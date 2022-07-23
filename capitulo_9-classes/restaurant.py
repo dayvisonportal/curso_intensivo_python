cd14454ca097f70e2c5eb24e73552f58e3025519
Dayvison Portal <dayvison.portal@gmail.com>
2022-07-23 15:40:29 -0300

    renomear_arquivos.py

---
+++ capitulo_9-classes/restaurant.py
@@ -0,0 +1,12 @@
+class Restaurant():
+    """Classe Restaurante"""
+    def __init__(self,restaurant_name,cuisine_type):
+        """Inicializa os atributos restaurant_name e cuisine_type."""
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant():
+        """Mostra o nome do restaurante e o tipo de cozinha."""
+    
+    def open_restaurant():
+        """Simula um informativo que o restaurante está aberto."""
